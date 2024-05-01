b178d7b844b7aff9e1f887c45525da972b7703a8
JohanVasquezM <vasquezmaldonadojohanisaac@gmail.com>
2024-05-01 13:58:52 -0600

    Agregar archivos de proyecto.

---
+++ Herencia-Clase/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace Herencia_Clase
+
+{
+    public class Animal
+    {
+       
+        public string Nombre { get; set; }
+
+        public string Color { get; set; }
+
+        public string Tamano { get; set; }
+
+        public Familia Familia { get; set; }
+
+        public Animal()
+        {
+            Familia = new Familia();
+        }
+
+    }
+}
