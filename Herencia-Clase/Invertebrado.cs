1dd595f9cc5879fb63f6ead63a682712afcc36da
JohanVasquezM <167148531+JohanVasquezM@users.noreply.github.com>
2024-05-01 15:02:51 -0600

    Ejercicio

--- Herencia-Clase/Invertebrado.cs
+++ Herencia-Clase/Invertebrado.cs
@@ -2,36 +2,21 @@
 {
     public class Invertebrado : Animal
     {
-        /// <summary>
-        /// Tipo de Animal
-        /// </summary>
+       
         public string Tipo { get; set; }
 
-        /// <summary>
-        /// Valor booleano que nos indica si el animal tiene patas o no
-        /// </summary>
         public bool TienePatas { get; set; }
 
-        /// <summary>
-        /// Valor entero que nos indica el número de patas de un animal
-        /// </summary>
         public int NumeroPatas { get; set; }
 
-        /// <summary>
-        /// Valor booleano que nos indica si el animal tiene concha o no
-        /// </summary>
         public bool TieneConcha { get; set; }
 
-        // Constructor
         public Invertebrado()
         {
             TienePatas = false;
             NumeroPatas = 0;
         }
 
-        /// <summary>
-        /// Imprime cada uno de los atributos de la clase
-        /// </summary>
         public void Imprimir()
         {
             Console.WriteLine($"Nombre: {Nombre}");
